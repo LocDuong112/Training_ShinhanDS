86997f394d355fc21d487cb21a03ef0ee7186772
DuongMinhLoc <duongminhloc201@gmail.com>
2023-02-18 11:41:06 +0700

    Loc | BUG: cannot modify body after request
    + Cannot write to body twice

--- Practice_config/src/main/java/com/example/practice_config/filter/BrowserDetectorFilter.java
+++ Practice_config/src/main/java/com/example/practice_config/filter/BrowserDetectorFilter.java
@@ -3,6 +3,7 @@ package com.example.practice_config.filter;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.apache.catalina.filters.AddDefaultCharsetFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -21,16 +22,28 @@ public class BrowserDetectorFilter implements Filter {
         String browser = httpServletRequest.getHeader("User-Agent");
 
         HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+//        ServletResponseWrapper responseWrapper = (ServletResponseWrapper) httpServletResponse;
         httpServletResponse.addHeader("Custom-Header", "This is a custom header");
         httpServletResponse.setContentType("application/json");
 
-        if (browser != null && browser.contains("Postman")) {
+        if (browser != null && false && browser.contains("Postman")) {
             httpServletResponse.setStatus(HttpServletResponse.SC_FORBIDDEN);
             String responseBody = "{ \"message\": \"You're requesting from Postman!\" }";
             httpServletResponse.getWriter().write(responseBody);
             httpServletResponse.getWriter().flush();
+
+            // I need to stop right here before calling to the controller
+            // So I dont call filterChain.doFilter()
         } else {
+            // This line will get the response
+            // If we write the body before this line it will cause the error
+            // I think we cannot write to the body 2 times for 1 request
             filterChain.doFilter(servletRequest, servletResponse);
+
+            // One way to modify the body is to use a wrapper
+//            String responseBody = "{ \"message\": \"You're requesting from " + browser + "\" }";
+//            responseWrapper.getWriter().write(responseBody);
+//            responseWrapper.getWriter().flush();
         }
 
         logger.info("Inside doFilter()");
